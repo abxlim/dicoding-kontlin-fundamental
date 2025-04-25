6ff5006fc4f4ef00cb57e374850e1417b8ff72f4
Abdul Halim <abxlim@users.noreply.github.com>
2025-04-25 20:05:57 +0700

    docs: menyelesaikan semua Exam
    Menyelesaikan Exam 2 hingga Exam 5

--- src/main/kotlin/com/dicoding/exam/exam3/Exam3.kt
+++ src/main/kotlin/com/dicoding/exam/exam3/Exam3.kt
@@ -2,5 +2,28 @@ package com.dicoding.exam.exam3
 
 // TODO
 fun <T> checkType(args: T): String {
-    return ""
+    val dataType: String
+
+    if (args is Int) {
+        dataType = "Integer"
+    }
+    else if (args is String) {
+        dataType = "String"
+    }
+    else if (args is Boolean) {
+        dataType = "Boolean"
+    }
+    else if (args is Double) {
+        dataType = "Double"
+    }
+    else if (args is List <*>) {
+        dataType = "List"
+    }
+    else if (args is Map <*, *>) {
+        dataType = "Map"
+    }
+    else {
+        dataType = "Unknown Type"
+    }
+    return "Yes! it's $dataType"
 }
